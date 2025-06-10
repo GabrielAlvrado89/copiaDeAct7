7b12b88620ab25897f34ff02cecb3710db02a45b
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2025-06-09 20:21:12 -0600

    Update firebase_services.dart

--- lib/Services/firebase_services.dart
+++ lib/Services/firebase_services.dart
@@ -9,7 +9,13 @@ Future<List> getPeople() async {
   for (var doc in queryPeople.docs) {
     final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     final person = {
+      "id": data['id'], // Agregado el campo 'id'
       "name": data['name'],
+      "age": data['age'], // Agregado el campo 'age'
+      "phone": data['phone'], // Agregado el campo 'phone'
+      "email": data['email'], // Agregado el campo 'email'
+      "address": data['address'], // Agregado el campo 'address'
+      "paymentMethod": data['paymentMethod'], // Agregado el campo 'paymentMethod'
       "uid": doc.id,
     };
     people.add(person);
@@ -17,19 +23,33 @@ Future<List> getPeople() async {
   return people;
 }
 
-// Guardar un name en base de datos
-Future<void> addPeople(String name) async {
-  await db.collection("people").add({"name": name});
+// Guardar un nuevo registro en base de datos con múltiples campos
+Future<void> addPeople(String id, String name, int age, String phone, String email, String address, String paymentMethod) async {
+  await db.collection("people").add({
+    "id": id,
+    "name": name,
+    "age": age,
+    "phone": phone,
+    "email": email,
+    "address": address,
+    "paymentMethod": paymentMethod,
+  });
 }
 
-//Actualizar un name en base de datos
-
-Future<void> updatePeople(String uid, String newName) async {
-  await db.collection("people").doc(uid).set({"name": newName});
+//Actualizar un registro en base de datos con múltiples campos
+Future<void> updatePeople(String uid, String newId, String newName, int newAge, String newPhone, String newEmail, String newAddress, String newPaymentMethod) async {
+  await db.collection("people").doc(uid).set({
+    "id": newId,
+    "name": newName,
+    "age": newAge,
+    "phone": newPhone,
+    "email": newEmail,
+    "address": newAddress,
+    "paymentMethod": newPaymentMethod,
+  });
 }
 
 // Borrar datos de la base de datos
-
 Future<void> deletePeople(String uid) async {
   await db.collection("people").doc(uid).delete();
 }
